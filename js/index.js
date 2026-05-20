f53f5838dff4b0de15467ad12b53a867617ab373
D3meLi7h <lagrotteriavaleria@gmail.com>
2026-05-20 19:30:00 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const riquadro = document.querySelector('.riquadro');
+
+// apertura popup
+const btnApri = document.querySelector('.btnLogin');
+btnApri.addEventListener('click', () =>
+{
+    riquadro.classList.add('attivaPopup');
+});
+
+// chiusura popup
+const btnChiudi = document.querySelector('.chiudi');
+btnChiudi.addEventListener('click', () =>
+{
+    riquadro.classList.remove('attivaPopup');
+});
+
+//pagina home
+const inputNickname = document.querySelector('.input-box input');
+const btnEntra = document.querySelector('.btnEntra');
+
+btnEntra.addEventListener('click', (event) =>
+{
+    event.preventDefault();
+    if(inputNickname.value.trim() === "")
+    {
+        alert("Riempi tutti i campi.");
+    }
+                
+    else
+    {
+        localStorage.setItem("nickname", inputNickname.value);
+        window.location.href = "include/generi.html";
+    }
+});
+
+//pagina registrazione
+const linkRegistra = document.querySelector('.linkRegistra');
+
+linkRegistra.addEventListener('click', (event) =>
+{
+    window.location.href = "include/registra.html";
+});
