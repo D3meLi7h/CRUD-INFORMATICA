0693995f05d698e1748937ae2b039f0f1490f742
DARX-XE <mattiapassamonte@gmail.com>
2026-05-26 23:36:05 +0200

    modifiche generali
    .

--- include/fantascienza.php
+++ include/fantascienza.php
@@ -1,43 +1,69 @@
-<html>
+<?php
+require_once __DIR__ . '/../php/db.php';
+
+$sql = "SELECT Libri.Titolo, Libri.Descrizione, Libri.Anno_Pubblicazione, Libri.Lingua_Originale, Libri.ISBN, Categorie.Genere_Libro
+        FROM Libri
+        JOIN Categorie ON Libri.Cod_Categoria = Categorie.Cod_Categoria
+        WHERE Categorie.Genere_Libro = 'Fantascienza'";
+
+$result = $conn->query($sql);
+$libriFantascienza = [];
+$errorMessage = null;
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) {
+        $libriFantascienza[] = $row;
+    }
+} else {
+    $errorMessage = $conn->error;
+}
+?>
+<!DOCTYPE html>
+<html lang="it">
     <head>
-        <title>Libreria</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Fantascienza</title>
         <link rel="stylesheet" href="../asset/css/generiLibri.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet">
     </head>
+    <body>
+        <div class="riquadro">
+            <h1 class="titolo">Fantascienza</h1>
+            <button class="btnLogout" id="logout">
+                <ion-icon name="exit-outline"></ion-icon>
+                <span class="nickname"></span>
+            </button>
+            <button class="btnIndietro" id="indietro">
+                <ion-icon name="arrow-back-outline"></ion-icon>
+            </button>
+            <h2 class="sottotitolo">My School Library</h2>
 
-<body>
+            <div class="contenuto-libro">
+                <?php if ($errorMessage): ?>
 
-    <!-- Contenitore principale della pagina -->
-    <div class="riquadro">
+                <p>Errore database: <?= htmlspecialchars($errorMessage) ?></p>
+            <?php elseif (count($libriFantascienza) === 0): ?>
+                <p>Nessun libro in categoria Fantascienza trovato nel database.</p>
+            <?php else: ?>
+                <h3>Libri nella categoria Fantascienza</h3>
+                <ul>
+                    <?php foreach ($libriFantascienza as $libro): ?>
+                        <li>
+                            <strong><?= htmlspecialchars($libro['Titolo']) ?></strong><br>
+                            <?= htmlspecialchars($libro['Descrizione']) ?><br>
+                            Anno: <?= htmlspecialchars($libro['Anno_Pubblicazione']) ?>, Lingua: <?= htmlspecialchars($libro['Lingua_Originale']) ?>, ISBN: <?= htmlspecialchars($libro['ISBN']) ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
+            </div>
+        </div>
 
-        <!-- Titolo principale della sezione -->
-        <h1 class="titolo">Fantascienza</h1>
-    
-        <!-- Bottone di logout -->
-        <button class="btnLogout" id="logout">
-            <!-- Icona Ionicons per logout -->
-            <ion-icon name="exit-outline"></ion-icon>
-
-            <!-- Inserimento nickname dell'utente nel bottone-->
-            <span class="nickname"></span>
-        </button>
-
-        <!-- Bottone per tornare alla pagina precedente -->
-        <button class="btnIndietro" id="indietro">
-            <!-- Icona freccia indietro -->
-            <ion-icon name="arrow-back-outline"></ion-icon>
-        </button>
-
-        <!-- Sottotitolo della pagina -->
-        <h2 class="sottotitolo">My School Library</h2>
-
-    </div>
-</body>
-
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-    <script src="../asset/js/bottoniGeneri.js"></script>
-
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        <script src="../asset/js/bottoniGeneri.js"></script>
+    </body>
 </html>
